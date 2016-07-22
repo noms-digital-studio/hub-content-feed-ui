2de38d4b7cb103d06f3867a4de38156de2b43b20
James Nicol <james.nicol@hpe.com>
2016-07-22 15:44:02 +0100

    MSS-77 hiding tags if empty

--- resources/views/video/detail.blade.php
+++ resources/views/video/detail.blade.php
@@ -8,7 +8,7 @@
   <div class="container">
     <div class="row">
       <div class="col-md-7">
-        <video id="video_player" class="video-js vjs-default-skin vjs-big-play-centered" controls preload="auto" width="600" height="240" data-setup='{}'>
+        <video id="video_player" class="video-js vjs-default-skin vjs-big-play-centered" controls preload="auto" width="600" height="240" data-setup='{}' controls poster="{{$video->getThumbnail()}}">
           <source src="{{ $video->getUrl() }}">
           <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
         </video>
@@ -22,6 +22,7 @@
         <div class="video-description">{!! $video->getDescription() !!}</div>
         <div class="video-duration">{{ $video->getDuration() }}</div>
 
+        @if($video->getTags())
         <div class="video-tags">
           <span class="label">Tags</span>
 
@@ -33,6 +34,7 @@
             @endforeach
           </ul>
         </div>
+        @endif
       </div>
     </div>
   </div>
