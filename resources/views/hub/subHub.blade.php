19d8ce006d3974378a6a5290893e9a91822ffef7
Michael <michaeljcrossman@icloud.com>
2016-07-29 10:34:11 +0100

    MSS-81 Updated hub templates to use dynamic folder paths

--- resources/views/hub/subHub.blade.php
+++ resources/views/hub/subHub.blade.php
@@ -13,14 +13,14 @@
 	<ul class="row row-centered">
 		@foreach($links as $link)
 		<li class="col-xs-3 col-centered">
-			@if($link->external_link)
+			@if($link->folder)
+			<a href="hub/{{ $link->tid }}" target="_blank" rel="noopener" id="term-{{ $link->tid }}">
+			@else
 			<a href="{{ $link->url }}" target="_blank" rel="noopener" id="term-{{ $link->tid }}">
-				@else
-				<a href="../{{ $link->url }}" target="_blank" rel="noopener" id="term-{{ $link->tid }}">
-					@endif
-					<img src="{{ $link->thumbnail }}" alt="" />
-					<h3>{{ $link->name }}</h3>
-				</a>
+			@endif
+				<img src="{{ $link->thumbnail }}" alt="" />
+				<h3>{{ $link->name }}</h3>
+			</a>
 		</li>
 		@endforeach
 	</ul>
