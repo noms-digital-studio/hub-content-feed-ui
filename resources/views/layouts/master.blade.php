3d73b818b5f3dc3e2c16cb221ed884ca4baf25c2
Alex Walton <alex.walton@orangebus.co.uk>
2016-07-29 09:59:07 +0100

    MSS-81 pulling updated master template

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,40 +1,41 @@
 <!DOCTYPE html>
 <html>
     <head>
-		<script src="/js/jquery-1.8.2.min.js"></script>
-		<script src="/js/bxslider/jquery.bxslider.min.js"></script>
-		<script src="/js/video.min.js"></script>
-		<script src="/js/global.js" type="text/javascript"></script>
-		<title>OICSS - @yield('title')</title>
-		<link href="{{ elixir('css/app.css') }}" rel="stylesheet" type="text/css" />
-		<link href="/css/sprite.css" rel="stylesheet" type="text/css" />
-		<link href="/js/bxslider/jquery.bxslider.css" rel="stylesheet" />
+    		<script src="/js/jquery-1.8.2.min.js"></script>
+    		<script src="/js/bxslider/jquery.bxslider.min.js"></script>
+        <script src="/js/video.min.js"></script>
+        <script src="/js/global.js" type="text/javascript"></script>
+
+        <title>OICSS - @yield('title')</title>
+        <link href="{{ elixir('css/app.css') }}" rel="stylesheet" type="text/css" />
+        <link href="/js/bxslider/jquery.bxslider.css" rel="stylesheet" />
+        <link href="/css/sprite.css" rel="stylesheet" type="text/css" />
     </head>
 
     <body>
-		@if (Request::is('/') || Request::is('hub/*')) 
-		@else
-        <div id="header">
-            <h1><a href="/">Content Store</a></h1>
+        @if (Request::is('/') || Request::is('hub/*'))
+        @else
+        <div class="container" id="header">
+            <h1><a href="/">The Hub</a></h1>
 
             <div class="row">
-				<div class="col-xs-12">
-					<ul class="nav-menu">
-						<li>
-							<h2><a href="/">Home</a></h2>
-						</li>
-						<li>
-							<h2><a href="/video" class="active">Video</a></h2>
-						</li>
-						</li>
-						<li>
-							<h2><a href="/radio">Radio</a></h2>
-						</li>
-					</ul>
-				</div>
+        				<div class="col-xs-12">
+          					<ul class="nav-menu">
+            						<li>
+            							  <h2><a href="/">Home</a></h2>
+            						</li>
+            						<li>
+            							  <h2><a href="/video" class="active">Video</a></h2>
+            						</li>
+            						</li>
+            						<li>
+    							          <h2><a href="/radio">Radio</a></h2>
+            						</li>
+          					</ul>
+        				</div>
             </div>
         </div>
-		@endif
+		    @endif
 
         @yield('top_content')
 
