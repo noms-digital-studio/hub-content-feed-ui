97acc7fcec88d5437a17ebdc0322710e62d1f1cc
Alex Walton <alex.walton@orangebus.co.uk>
2016-08-01 11:31:04 +0100

    Translating episode links

--- resources/views/video/episodeCard.blade.php
+++ resources/views/video/episodeCard.blade.php
@@ -6,7 +6,7 @@
 
     <div class="overlay">
         <span class="programme-name">{{ $episode->getChannel() }}</span>
-        <a href="/video/{{ $episode->getId() }}" id="programme-{{ $episode->getId() }}">
+        <a href="{{ action('VideosController@show', $episode->getId()) }}" id="programme-{{ $episode->getId() }}">
             <h4>Play {{ $episode->getTitle() }}</h4>
         </a>
     </div>
