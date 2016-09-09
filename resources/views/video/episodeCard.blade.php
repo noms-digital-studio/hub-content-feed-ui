6eb116fe964b3112e7caf9326041787142ab4a1a
Michael <michaeljcrossman@icloud.com>
2016-09-09 11:45:22 +0100

    MCT-9 Updated frontend to use thumbnail image style, delivered from api

--- resources/views/video/episodeCard.blade.php
+++ resources/views/video/episodeCard.blade.php
@@ -1,13 +1,17 @@
 <div class="card programme-card episode-card">
-    <img src="{{  $episode->getThumbnail() }}">
-    <div class="programme-title">
-      <h6>{{ $episode->getTitle() }}</h6>
-    </div>
+	<div class="shadow">
+		<img src="{{  $episode->getThumbnail() }}">
+		<span class="icon icon-play-button-white"></span>
+		<div class="programme-title">
+			<h6>{{ $episode->getTitle() }}</h6>
+			<span class="duration"><span class="icon icon-icon-clock"></span>{{ $episode->getDuration() }}</span>
+		</div>
 
-    <div class="overlay">
-        <span class="programme-name">{{ $episode->getChannel() }}</span>
-        <a href="{{ action('VideosController@show', $episode->getId()) }}" id="programme-{{ $episode->getId() }}">
-            <h4>Play {{ $episode->getTitle() }}</h4>
-        </a>
-    </div>
+		<div class="overlay">        
+			<a href="{{ action('VideosController@show', $episode->getId()) }}" id="programme-{{ $episode->getId() }}">
+				<span class="programme-name">{{ $episode->getChannel() }}</span>
+				<h4><span class="icon icon-play-button"></span>{{ trans('video.play') }}:<br />{{ $episode->getTitle() }}</h4>
+			</a>
+		</div>
+	</div>	
 </div>
--- resources/views/video/programmeCard.blade.php
+++ resources/views/video/programmeCard.blade.php
@@ -1,13 +1,15 @@
 <div class="card programme-card">
-    <img src="{{ $programme->episodes->thumbnail }}">
-    <div class="programme-title">
-      <h6>{{ $programme->title }}</h6>
-    </div>
+	<div class="shadow">
+		<img src="{{ $programme->episodes->thumbnail }}">
+		<div class="programme-title">
+			<h6>{{ $programme->title }}</h6>
+		</div>
 
-    <div class="overlay">
-        <span class="programme-name">{{ $programme->title }}</span>
-        <a href="{{ action('VideosController@show', $programme->episodes->nid) }}" id="programme-{{ $programme->episodes->nid }}">
-            <h4>{{ trans('video.play') }} {{ $programme->episodes->title }}</h4>
-        </a>
-    </div>
+		<div class="overlay">
+			<a href="{{ action('VideosController@show', $programme->episodes->nid) }}" id="programme-{{ $programme->episodes->nid }}">
+				<span class="programme-name">{{ $programme->title }}</span>			
+				<h4><span class="icon icon-play-button"></span>{{ trans('video.play') }}:<br /> {{ $programme->episodes->title }}</h4>
+			</a>
+		</div>
+	</div>
 </div>
