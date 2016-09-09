215ede222dde5fb27b1cf6ac15db67657ab03344
Alex Walton <alex.walton@orangebus.co.uk>
2016-09-09 15:53:44 +0100

    MSS-140 Fixing accessibility errors on video detail page

--- resources/views/video/programmeCard.blade.php
+++ resources/views/video/programmeCard.blade.php
@@ -1,6 +1,6 @@
 <div class="card programme-card">
 	<div class="shadow">
-		<img src="{{ $programme->episodes->thumbnail }}" alt="{{ $programme->title() }}">
+		<img src="{{ $programme->episodes->thumbnail }}" alt="{{ $programme->title }}">
 		<div class="programme-title">
 			<h6>{{ $programme->title }}</h6>
 		</div>
@@ -8,7 +8,7 @@
 		<div class="overlay">
 			<a href="{{ action('VideosController@show', $programme->episodes->nid) }}" id="programme-{{ $programme->episodes->nid }}">
 				<span class="programme-name">{{ $programme->title }}</span>
-				<h4><span class="icon icon-play-button"></span>{{ trans('video.play') }}:<br /> {{ $programme->episodes->title }}</h4>
+				<h4><span class="icon icon-play-button" aria-hidden="true"></span>{{ trans('video.play') }}:<br /> {{ $programme->episodes->title }}</h4>
 			</a>
 		</div>
 	</div>
