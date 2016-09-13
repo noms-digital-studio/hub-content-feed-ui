65381589a05d2f5e18c7f9f7dfa795962e12f070
James Nicol <james.nicol@hpe.com>
2016-09-13 14:45:00 +0100

    MSS-146 Moved styles from 404.blade to a scss file.

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,68 +1,19 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>404 Error</title>
+@extends('layouts.master')
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+@section('title', '404 Error')
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+@section('content')
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                color: #000;
-                display: table;
-                font-weight: 100;
-                font-family: "Open Sans", sans-serif;
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 180px;
-                margin-top: -200px;
-                margin-bottom: 15px;
-                color: #cccccc;
-                display: inline;
-            }
-
-            .error {
-                font-weight: bold;
-                font-size: 25px;
-            }
-
-            #back {
-                border: 1px solid #b3b3b3;
-                padding: 10px 23px;
-                background: #fff;
-                border-radius: 25px;
-                cursor: pointer;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
+        <div class="error-container">
+          <div class="col-xs-12">
             <div class="content">
-                <div class="title">4</div>
-                <img src="/img/icon-hub-medium.png">
-                <div class="title">4</div>
-                <div class ="error">Page not found</div>
-                <p>The page you are looking for cannot be found.</p>
-                <a href ="/"><button id="back">{{ trans('navigation.title') }}</button</a>
+                  <div class="title">4</div>
+                  <img src="/img/icon-hub-medium.png">
+                  <div class="title">4</div>
+                  <div class ="error">Page not found</div>
+                  <p>The page you are looking for cannot be found.</p>
+                  <a href ="/"><button id="back">{{ trans('navigation.title') }}</button</a>
             </div>
+          </div>
         </div>
-    </body>
-</html>
+	@endsection
