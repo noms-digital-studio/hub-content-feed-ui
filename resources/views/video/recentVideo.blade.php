e6979209489a738512bd5b3934e86d09d860538d
James Nicol <james.nicol@hpe.com>
2016-09-14 12:04:38 +0100

    MSS-145 Added the blue background, border-radius and play button icon to carousel.

--- resources/views/video/recentVideo.blade.php
+++ resources/views/video/recentVideo.blade.php
@@ -1,21 +1,24 @@
 	<ul class="bxslider">
 		@foreach($recentVideos as $video)
 		<li>
-			<div class="row">
-				<div class="col-xs-4">
-					<a href="{{ action('VideosController@show', $video->getId()) }}">
-						<img src="{{ $video->getThumbnail() }}" alt="" />
-					</a>
-				</div>
+			<div class="content-wrap">
+					<div class="row">
+							<div class="col-xs-6">
+									<a href="{{ action('VideosController@show', $video->getId()) }}">
+										<span class="icon icon-play-button-white"></span>
+											<img src="{{ $video->getThumbnail() }}" alt="" />
+									</a>
+							</div>
 
-				<div class="col-xs-7">
-					<p>{{ $video->getChannel() }}</p>
-					<h2>{{ $video->getTitle() }}</h2>
-					<p>{!! $video->getDescription() !!}</p>
-					@if($video->getDuration())
-						<div class="video-duration">{{ $video->getDuration() }}</div>
-					@endif
-				</div>
+							<div class="col-xs-6">
+									<p>{{ $video->getChannel() }}</p>
+									<h2>{{ $video->getTitle() }}</h2>
+									<p>{!! $video->getDescription() !!}</p>
+									@if($video->getDuration())
+											<div class="video-duration">{{ $video->getDuration() }}</div>
+									@endif
+							</div>
+					</div>
 			</div>
 		</li>
 		@endforeach
