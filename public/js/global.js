8d49bd235a5d45d1396c12e9ace35e7198dff738
Michael <michaeljcrossman@icloud.com>
2016-10-07 13:56:14 +0100

    MSS-158 MSS-159 Analytics tracking events triggered by video and radio player

--- public/js/global.js
+++ public/js/global.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
     $('.bxslider').bxSlider();
     $("#AboutInfo").hide();
     $("#EpisodeLink").click(function (e) {
@@ -15,146 +16,239 @@ $(document).ready(function () {
         $("#EpisodeLink").removeClass("active");
         $(this).addClass("active");
     });
+
 });
 
 (function ($) {
-
     $(function () {
 
-        var setup = {
-            'controls': true,
-            'controlBar': {
-                'children': {
-                    'muteToggle': {},
-                    'playToggle': {},
-                    'volumeControl': {},
-                    'durationDisplay': {},
-                    'currentTimeDisplay': {},
-                    'progressControl': {
-                        'children': {
-                            'seekBar': {
-                                'children': {
-                                    'playProgressBar': {}
+        var videoPlayerId = '#video_player';
+        var radioPlayerId = '#radio-player';
+        var showTitle = '';
+        var playerCategory = '';
+
+        //Check if video player exists on page
+        if ($(videoPlayerId).length) {
+
+            playerCategory = "Video";
+            showTitle = '.video-title';
+
+            //Set up videoPlayer
+            var player = videojs('#video_player', {}, function () {
+            });
+
+            //Check if radio player exists on page
+        } else if ($(radioPlayerId).length) {
+
+            playerCategory = "Radio";
+            showTitle = '#radio-player-title';
+            var setup = {
+                'controls': true,
+                'controlBar': {
+                    'children': {
+                        'muteToggle': {},
+                        'playToggle': {},
+                        'volumeControl': {},
+                        'durationDisplay': {},
+                        'currentTimeDisplay': {},
+                        'progressControl': {
+                            'children': {
+                                'seekBar': {
+                                    'children': {
+                                        'playProgressBar': {}
+                                    }
                                 }
                             }
                         }
                     }
                 }
-            }
-        };
+            };
+
+            //Set up radio player
+            var player = videojs('#radio-player', setup, function () {
+            });
 
 
-//        TODO - move repetative stuff in to functions - make better during the ticket to style radio player
-//          TODO - remove from global.js - only load on radio player page.
+            $('[data-audio-src]').on('click.play-radio-show', function (e) {
+                e.preventDefault();
+                var src = $(this).data('audio-src');
+                var title = $(this).data('audio-title');
+                var currentShow = $('[data-audio-src="' + src + '"]').parent();
 
-        var player = videojs('#radio-player', setup, function () {
-        });
+                if (!currentShow.next().length) {
+                    $('#play-next-show span').addClass('icon-next-small-grey').removeClass('icon-next-small');
+                } else {
+                    $('#play-next-show span').addClass('icon-next-small').removeClass('icon-next-small-grey');
+                }
+                if (!currentShow.prev().length) {
+                    $('#play-prev-show span').addClass('icon-prev-small-grey').removeClass('icon-prev-small');
+                } else {
+                    $('#play-prev-show span').addClass('icon-prev-small').removeClass('icon-prev-small-grey');
+                }
+                if ($(this).hasClass('moj-audio-paused')) {
+                    player.play();
+                } else if ($(this).hasClass('moj-audio-playing')) {
+                    player.pause();
+                } else {
+                    player.src([
+                        {type: 'audio/mp3', src: src}
+                    ]).play();
 
-        $('[data-audio-src]').on('click.play-radio-show', function (e) {
-            e.preventDefault();
-            var src = $(this).data('audio-src');
-            var title = $(this).data('audio-title');
-            var currentShow = $('[data-audio-src="' + src + '"]').parent();
-            if (!currentShow.next().length) {
-                $('#play-next-show span').addClass('icon-next-small-grey').removeClass('icon-next-small');
-            } else {
-                $('#play-next-show span').addClass('icon-next-small').removeClass('icon-next-small-grey');
-            }
-            if (!currentShow.prev().length) {
-                $('#play-prev-show span').addClass('icon-prev-small-grey').removeClass('icon-prev-small');
-            } else {
-                $('#play-prev-show span').addClass('icon-prev-small').removeClass('icon-prev-small-grey');
-            }
-            if ($(this).hasClass('moj-audio-paused')) {
-                player.play();
-            } else if ($(this).hasClass('moj-audio-playing')) {
-                player.pause();
-            } else {
-                player.src([
-                    {type: 'audio/mp3', src: src}
-                ]).play();
-                
-                $('#radio-player-title').text(title);
-            }
-        });
+                    $('#radio-player-title').text(title);
+                }
+            });
 
-        $('#play-next-show').on('click', function () {
-            var src = player.src();
-            var currentShow = $('[data-audio-src="' + src + '"]').parent();
-            var nextShow = currentShow.next();
-            nextShow.find('a').click();
-        });
+            $('#play-next-show').on('click', function () {
+                var src = player.src();
+                var currentShow = $('[data-audio-src="' + src + '"]').parent();
+                var nextShow = currentShow.next();
+                nextShow.find('a').click();
+            });
 
-        $('#play-prev-show').on('click', function () {
-            var src = player.src();
-            var currentShow = $('[data-audio-src="' + src + '"]').parent();
-            var prevShow = currentShow.prev();
-            prevShow.find('a').click();
-        });
+            $('#play-prev-show').on('click', function () {
+                var src = player.src();
+                var currentShow = $('[data-audio-src="' + src + '"]').parent();
+                var prevShow = currentShow.prev();
+                prevShow.find('a').click();
+            });
 
-        player.on('play', function () {
-            var src = player.src();
-            $('.radio-container ul li a').removeClass('moj-audio-paused moj-audio-playing');
-            $('.radio-container ul li a span.play-show-icon-clock').removeClass('icon-clock-white').addClass('icon-clock');
+            player.on('play', function () {
+                var src = player.src();
+                $('.radio-container ul li a').removeClass('moj-audio-paused moj-audio-playing');
+                $('.radio-container ul li a span.play-show-icon-clock').removeClass('icon-clock-white').addClass('icon-clock');
 
-            //Remove pause button from every row and add a play button
-            $('.radio-container ul li a span.play-show-button').removeClass('icon-pause-button-white').addClass('icon-play-button');
+                //Remove pause button from every row and add a play button
+                $('.radio-container ul li a span.play-show-button').removeClass('icon-pause-button-white').addClass('icon-play-button');
 
-            var selectedShow = $('[data-audio-src="' + src + '"]');
-            selectedShow.addClass('moj-audio-playing');
+                var selectedShow = $('[data-audio-src="' + src + '"]');
+                selectedShow.addClass('moj-audio-playing');
 
-            //Add a white pause button to the row which is playing
-            selectedShow.find('span.play-show-button').removeClass('icon-play-button').removeClass('icon-play-button-white').addClass('icon-pause-button-white');
+                //Add a white pause button to the row which is playing
+                selectedShow.find('span.play-show-button').removeClass('icon-play-button').removeClass('icon-play-button-white').addClass('icon-pause-button-white');
 
-            $('.radio-container ul li').removeClass('show-playing');
-            selectedShow.parent().addClass('show-playing');
+                $('.radio-container ul li').removeClass('show-playing');
+                selectedShow.parent().addClass('show-playing');
 
-            //Change the highlighted rows clock to white
-            selectedShow.find('.icon.play-show-icon-clock').addClass('icon-clock-white').removeClass('icon-clock');
-        });
+                //Change the highlighted rows clock to white
+                selectedShow.find('.icon.play-show-icon-clock').addClass('icon-clock-white').removeClass('icon-clock');
+            });
 
-        player.on('pause', function (e) {
-            var src = player.src();
-            $('.radio-container ul li a').removeClass('moj-audio-paused moj-audio-playing');
-            $('[data-audio-src="' + src + '"]').addClass('moj-audio-paused');
-            var selectedShow = $('[data-audio-src="' + src + '"]');
+            player.on('pause', function (e) {
+                var src = player.src();
+                $('.radio-container ul li a').removeClass('moj-audio-paused moj-audio-playing');
+                $('[data-audio-src="' + src + '"]').addClass('moj-audio-paused');
+                var selectedShow = $('[data-audio-src="' + src + '"]');
 
-            //Remove pause button and replace with white play button
-            selectedShow.find('.icon.icon-pause-button-white').removeClass('icon-pause-button-white').addClass('icon-play-button-white');
-        });
+                //Remove pause button and replace with white play button
+                selectedShow.find('.icon.icon-pause-button-white').removeClass('icon-pause-button-white').addClass('icon-play-button-white');
+            });
 
-        player.on('timeupdate', function (e){
-          var progressPosition = $('.vjs-progress-control .vjs-slider-horizontal .vjs-play-progress').width();
-          var currentTime = progressPosition - 27;
-          var duration = progressPosition;
+            player.on('timeupdate', function (e) {
 
-          $('.vjs-current-time').css({'left': currentTime + 'px'});
-          $('.vjs-duration').css({'left': duration + 'px'});
-      });
+                var progressPosition = $('.vjs-progress-control .vjs-slider-horizontal .vjs-play-progress').width();
+                var currentTime = progressPosition - 27;
+                var duration = progressPosition;
 
-        //Highlight selected show on page load
-        var src = player.src();
-        var selectedShow = $('[data-audio-src="' + src + '"]');
-        selectedShow.parent().addClass('show-playing');
-        selectedShow.find('.icon.icon-clock').addClass('icon-clock-white').removeClass('icon-clock');
-        selectedShow.find('.icon.icon-play-button').addClass('icon-play-button-white').removeClass('icon-play-button');
+                $('.vjs-current-time').css({'left': currentTime + 'px'});
+                $('.vjs-duration').css({'left': duration + 'px'});
+            });
 
-        if (!selectedShow.parent().next().length) {
-            $('#play-next-show span').addClass('icon-next-small-grey').removeClass('icon-next-small');
+
+            //Highlight selected show on page load - don't think this works anymore
+            player.on('ready', function () {
+                var src = player.src();
+                var selectedShow = $('[data-audio-src="' + src + '"]');
+                selectedShow.parent().addClass('show-playing');
+                selectedShow.find('.icon.icon-clock').addClass('icon-clock-white').removeClass('icon-clock');
+                selectedShow.find('.icon.icon-play-button').addClass('icon-play-button-white').removeClass('icon-play-button');
+
+                if (!selectedShow.parent().next().length) {
+                    $('#play-next-show span').addClass('icon-next-small-grey').removeClass('icon-next-small');
+                }
+
+                if (!selectedShow.parent().prev().length) {
+                    $('#play-prev-show span').addClass('icon-prev-small-grey').removeClass('icon-prev-small');
+                }
+
+                //Position player button/icons on the screen
+                var controlsPosition = $('#play-next-show').position().top;
+                var playPosition = controlsPosition - 5;
+
+                var volumePosition = controlsPosition + 70;
+                var mutePosition = controlsPosition + 57;
+
+                $('.vjs-play-control').css({'top': playPosition + 'px'});
+                $('.vjs-volume-control').css({'top': volumePosition + 'px'});
+                $('.vjs-mute-control').css({'top': mutePosition + 'px'});
+            });
         }
 
-        if (!selectedShow.parent().prev().length) {
-          $('#play-prev-show span').addClass('icon-prev-small-grey').removeClass('icon-prev-small');
+        //If a radio or video player exists on page, send analytics data
+        if (player) {
+
+            var twentyFiveSent = false;
+            var fiftySent = false;
+            var seventyFiveSent = false;
+            var ninetySent = false;
+
+
+            function resetAnalyticsVariables() {
+
+                twentyFiveSent = false;
+                fiftySent = false;
+                seventyFiveSent = false;
+                ninetySent = false;
+            }
+
+
+            //Reset analytics variables when player source changes
+            player.on('loadstart', function () {
+
+                resetAnalyticsVariables();
+            });
+
+
+            //Reset analytics variables when player ends
+            player.on('ended', function () {
+
+                resetAnalyticsVariables();
+            });
+
+
+            player.on('timeupdate', function (e) {
+
+                var showDuration = player.duration();
+                var title = $(showTitle).text();
+
+                //Determine percentage of radio show played
+                var pct = (player.currentTime() / showDuration);
+
+                //Send analytics data to PIWIK
+                if (pct > 0.25 && twentyFiveSent === false) {
+                    twentyFiveSent = true;
+                    _paq.push(['trackEvent', playerCategory, '25%', title]);
+                    console.log('25% ' + playerCategory + ' event sent');
+                }
+
+                if (pct > 0.5 && fiftySent === false) {
+                    fiftySent = true;
+                    _paq.push(['trackEvent', playerCategory, '50%', title]);
+                    console.log('50% ' + playerCategory + ' event sent');
+                }
+
+                if (pct > 0.75 && seventyFiveSent === false) {
+                    seventyFiveSent = true;
+                    _paq.push(['trackEvent', playerCategory, '75%', title]);
+                    console.log('75% ' + playerCategory + ' event sent');
+                }
+
+                if (pct > 0.9 && ninetySent === false) {
+                    ninetySent = true;
+                    _paq.push(['trackEvent', playerCategory, '90%', title]);
+                    console.log('90% ' + playerCategory + ' event sent');
+                }
+            });
         }
 
-        //Position player button/icons on the screen
-        var controlsPosition = $('#play-next-show').position().top;
-        var playPosition = controlsPosition - 5;
-        var volumePosition = controlsPosition + 70;
-        var mutePosition = controlsPosition + 57;
-
-        $('.vjs-play-control').css({'top': playPosition + 'px'});
-        $('.vjs-volume-control').css({'top': volumePosition + 'px'});
-        $('.vjs-mute-control').css({'top': mutePosition + 'px'});
     });
 }(jQuery));
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="{{ LaravelLocalization::getCurrentLocale() }}">
-    <head> 
+    <head> 				
 		<script src="/js/jquery-1.8.2.min.js"></script>
 		<script src="/js/bxslider/jquery.bxslider.min.js"></script>
-        <script src="/js/video.min.js"></script>
-        <script src="/js/global.js" type="text/javascript"></script>
-        <script src="/js/jquery.modal.min.js" type="text/javascript" charset="utf-8"></script>
-
-        <title>OICSS - @yield('title')</title>
+        <script src="/js/video.min.js"></script>		
+        <script src="/js/global.js" type="text/javascript"></script>		
+        <script src="/js/jquery.modal.min.js" type="text/javascript" charset="utf-8"></script>				
+		<title>OICSS - @yield('title')</title>
         <link href="{{ elixir('css/app.css') }}" rel="stylesheet" type="text/css" />
         <link href="/js/bxslider/jquery.bxslider.css" rel="stylesheet" />
         <link href="/css/sprite.css" rel="stylesheet" type="text/css" />
+		{!! App\Helpers\Piwik::trackingCode() !!}
     </head>
 
     <body>
@@ -24,5 +24,8 @@
         <div id="content" class="container">
             @yield('content')
         </div>
+
+		
+
     </body>
 </html>
