2375d05086c7c927e4e110a66ffda901c76849f5
James Nicol <james.nicol@hpe.com>
2016-10-07 14:50:14 +0100

    MSS-178 Included bootstrap dropdown menu. Dropdown styled to match designs

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="{{ LaravelLocalization::getCurrentLocale() }}">
     <head> 
-		<script src="/js/jquery-1.8.2.min.js"></script>
+		<script src="/js/jquery-1.12.4.min.js"></script>
 		<script src="/js/bxslider/jquery.bxslider.min.js"></script>
         <script src="/js/video.min.js"></script>
         <script src="/js/global.js" type="text/javascript"></script>
         <script src="/js/jquery.modal.min.js" type="text/javascript" charset="utf-8"></script>
+        <script src="/js/bootstrap.js" type="text/javascript"></script>
 
         <title>OICSS - @yield('title')</title>
         <link href="{{ elixir('css/app.css') }}" rel="stylesheet" type="text/css" />
--- resources/views/stickyNavigation.blade.php
+++ resources/views/stickyNavigation.blade.php
@@ -14,6 +14,17 @@
                     {{ $title }}
                 </a>
             </div>
+            <div class="dropdown">
+                <button class="language-button" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                    English
+                    <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                    <li><a href="#">English</a></li>
+                    <li><a href="#">Welsh</a></li>
+                </ul>
+            </div>
+
         </div>
     </div>
 </div>
