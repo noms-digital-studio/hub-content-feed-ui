677ce15cf0b405073cbe94ccb99a51906f8e49e6
James Nicol <james.nicol@hpe.com>
2016-10-13 14:29:09 +0100

    MSS-195 Removed carousel from Video header

--- resources/lang/en/video.php
+++ resources/lang/en/video.php
@@ -6,4 +6,5 @@ return [
   'episodes' => 'Episodes',
   'about' => 'About',
   'newvideo' => 'New video',
+  'description' => 'Welcome to the video section. Please select a video programme from the list of channels below where you will be able to select and start watching programme episodes. Our programme episodes will be updated on a regular basis.',
 ];
