82055adc9a94751e7417299e399402a288d56dbf
James Nicol <james.nicol@hpe.com>
2016-10-18 13:54:20 +0100

    MSS-200 corrected styling for play icon and duration info on thumbnail

--- resources/views/video/episodeCard.blade.php
+++ resources/views/video/episodeCard.blade.php
@@ -1,11 +1,13 @@
 <div class="card programme-card episode-card">
 	<div class="shadow">
 		<a href="{{ action('VideosController@show', $episode->getId()) }}" id="programme-{{ $episode->getId() }}">
-			<img src="{{ $episode->getThumbnail() }}" alt="{{ $episode->getTitle() }}">
-			<span class="icon icon-play-button-white" aria-hidden="true"></span>
-			@if($episode->getDuration())
+			<div class="card-thumbnail">
+				<img src="{{ $episode->getThumbnail() }}" alt="{{ $episode->getTitle() }}">
+				<span class="icon icon-play-button-white" aria-hidden="true"></span>
+				@if($episode->getDuration())
 					<span class="duration"><span class="icon icon-icon-clock" aria-hidden="true"></span>{{ $episode->getDuration() }}</span>
 				@endif
+			</div>
 			<div class="programme-title">
 				<h6>{{ $episode->getTitle() }}</h6>
 			</div>
