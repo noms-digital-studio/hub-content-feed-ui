8ddd1ae4bccc6252b64f3fda928da028d3543d40
James Nicol <james.nicol@hpe.com>
2016-10-18 11:34:23 +0100

    MSS-200 Updated video landing page thumbnail hover state and fixed play icon on detial page

--- resources/views/video/episodeCard.blade.php
+++ resources/views/video/episodeCard.blade.php
@@ -1,19 +1,14 @@
 <div class="card programme-card episode-card">
 	<div class="shadow">
-		<img src="{{ $episode->getThumbnail() }}" alt="{{ $episode->getTitle() }}">
-		<span class="icon icon-play-button-white" aria-hidden="true"></span>
-		@if($episode->getDuration())
-				<span class="duration"><span class="icon icon-icon-clock" aria-hidden="true"></span>{{ $episode->getDuration() }}</span>
-			@endif
-		<div class="programme-title">
-			<h6>{{ $episode->getTitle() }}</h6>
-		</div>
-
-		<div class="overlay">        
-			<a href="{{ action('VideosController@show', $episode->getId()) }}" id="programme-{{ $episode->getId() }}">
-				<span class="programme-name">{{ $episode->getChannel() }}</span>
-				<h4><span class="icon icon-play-button" aria-hidden="true"></span>{{ trans('video.play') }}:<br />{{ $episode->getTitle() }}</h4>
-			</a>
-		</div>
+		<a href="{{ action('VideosController@show', $episode->getId()) }}" id="programme-{{ $episode->getId() }}">
+			<img src="{{ $episode->getThumbnail() }}" alt="{{ $episode->getTitle() }}">
+			<span class="icon icon-play-button-white" aria-hidden="true"></span>
+			<!--@if($episode->getDuration())
+					<span class="duration"><span class="icon icon-icon-clock" aria-hidden="true"></span>{{ $episode->getDuration() }}</span>
+				@endif-->
+			<div class="programme-title">
+				<h6>{{ $episode->getTitle() }}</h6>
+			</div>
+		</a>
 	</div>
 </div>
