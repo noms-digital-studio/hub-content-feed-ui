69d7484849966c7782fdbc1ee0d288b4dad8e9d4
James Nicol <james.nicol@hpe.com>
2016-10-18 12:07:05 +0100

    MSS-200 hover state for video detail page, fixed duration style problem

--- resources/views/video/episodeCard.blade.php
+++ resources/views/video/episodeCard.blade.php
@@ -3,9 +3,9 @@
 		<a href="{{ action('VideosController@show', $episode->getId()) }}" id="programme-{{ $episode->getId() }}">
 			<img src="{{ $episode->getThumbnail() }}" alt="{{ $episode->getTitle() }}">
 			<span class="icon icon-play-button-white" aria-hidden="true"></span>
-			<!--@if($episode->getDuration())
+			@if($episode->getDuration())
 					<span class="duration"><span class="icon icon-icon-clock" aria-hidden="true"></span>{{ $episode->getDuration() }}</span>
-				@endif-->
+				@endif
 			<div class="programme-title">
 				<h6>{{ $episode->getTitle() }}</h6>
 			</div>
