dbb249d2740cc1ea8e452909a98c317e511d5681
Michael <michaeljcrossman@icloud.com>
2016-10-24 12:11:54 +0100

    Removed language dropdown from stickyNav added to its own template

---
+++ resources/views/languageDropdown.blade.php
@@ -0,0 +1,11 @@
+<div class="dropdown">
+	<button class="language-button" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+		English
+		<span class="icon icon-dropdown"></span>
+	</button>
+	<ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+		<li><a href="#">English</a></li>
+		<li><a href="#">Welsh</a></li>
+	</ul>
+</div>
+
--- resources/views/stickyNavigation.blade.php
+++ resources/views/stickyNavigation.blade.php
@@ -13,18 +13,7 @@
                     <span class="icon {{ $icon }}" aria-hidden="true"></span>
                     {{ $title }}
                 </a>
-            </div>
-            <div class="dropdown">
-                <button class="language-button" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                    English
-                    <span class="icon icon-dropdown"></span>
-                </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                    <li><a href="#">English</a></li>
-                    <li><a href="#">Welsh</a></li>
-                </ul>
-            </div>
-
+            </div>            
         </div>
     </div>
 </div>
