3076555426c6c73dda75dd38a491e53870bda4da
James Nicol <james.nicol@hpe.com>
2016-11-14 08:57:56 +0000

    Removing potential // from PIWIK url so PIWIK can record data

--- app/Helpers/Piwik.php
+++ app/Helpers/Piwik.php
@@ -13,14 +13,14 @@ class Piwik {
                 _paq.push(['trackPageView']);
                 _paq.push(['enableLinkTracking']);
                 (function() {
-                var u=\"//" . $piwikUrl . "/\";
+                var u=\"" . $piwikUrl . "/\";
                 _paq.push(['setTrackerUrl', u+'piwik.php']);
                 _paq.push(['setSiteId', '1']);
                 var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
                 g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
                 })();
                 </script>
-                <noscript><p><img src=\"//" . $piwikUrl . "/piwik.php?idsite=1\" style=\"border:0;\" alt=\"\" /></p></noscript>
+                <noscript><p><img src=\"" . $piwikUrl . "/piwik.php?idsite=1\" style=\"border:0;\" alt=\"\" /></p></noscript>
                 <!-- End Piwik Code -->";
         } else {
             return "<script type=\"text/javascript\">
